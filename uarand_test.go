edaa5e6b8d6762db4de931201b74859881bb5bfc
Dmitry Moskowski <me@corpix.ru>
2019-08-26 20:50:53 +0000

    uarand: added mutex to getrandom
    fixing #4

--- uarand_test.go
+++ uarand_test.go
@@ -11,3 +11,21 @@ func TestGetRandom(t *testing.T) {
 		assert.NotEqual(t, "", GetRandom())
 	}
 }
+
+func TestGetRandomConcurrent(t *testing.T) {
+	var (
+		n = 0
+		ch = make(chan string)
+	)
+
+
+	for k := 0; k < len(UserAgents)*10; k++ {
+		go func() { ch <- GetRandom() }()
+		n++
+	}
+
+	for n > 0 {
+		assert.NotEqual(t, "", <-ch)
+		n--
+	}
+}
