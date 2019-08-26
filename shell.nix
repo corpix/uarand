6fd8ff1ca6b2b41116c4bb7571662b9762ae62d6
Dmitry Moskowski <me@corpix.ru>
2019-08-26 21:34:12 +0000

    shell: added python3
    to update useragents in pure environment

--- shell.nix
+++ shell.nix
@@ -8,7 +8,7 @@ stdenv.mkDerivation {
   name = "nix-shell";
   buildInputs = [
     coreutils bashInteractive jq curl gcc git gnumake
-    go golangci-lint cacert openssl
+    go golangci-lint cacert openssl python3
   ];
   shellHook = ''
     unset GOPATH
