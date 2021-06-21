8c6d4bdf90a30a524821cb4bfb840f288aed55e9
Dmitry Moskowski <me@corpix.ru>
2021-06-21 23:10:54 +0000

    use nixpkgs from system

--- shell.nix
+++ shell.nix
@@ -1,8 +1,5 @@
 let
-  nixpkgs = builtins.fetchTarball {
-    url    = "https://github.com/nixos/nixpkgs/archive/ddf38a8241089d79c3bcd1777781b6438ab88d84.tar.gz";
-    sha256 = "0fjk69mn58h0gjzgxgnkfkhhf1l707bg2cn4823ma9xbjxbhl0ya";
-  };
+  nixpkgs = <nixpkgs>;
 in with import nixpkgs {};
 stdenv.mkDerivation {
   name = "nix-shell";
